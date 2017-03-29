1311c358fa7f475747ead796c31432d2fa227d86
Saikat Ghosh <saikat.ghosh.92505@gmail.com>
2017-03-29 12:40:55 +0530

    Update mysqli-connection-and-query.php

--- mysqli-connection-and-query.php
+++ mysqli-connection-and-query.php
@@ -1,5 +1,5 @@
 <?php
 
-	$link = mysqli_connect('localhost','root','','database') or die(mysql_error());
+	$link = mysqli_connect('host','username','password','database_name') or die(mysql_error());
 	mysqli_query($link,"SELECT * FROM 'tablename'");
 ?>
